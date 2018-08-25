ab7511057c3ced50553d53e8ac5c434a5fd4f29f
ANS <36783010+answebdev@users.noreply.github.com>
2018-08-24 20:31:22 -0700

    code updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -127,7 +127,7 @@ document.onkeyup = function (event) {
     console.log(wrongLetter);
 
     if (guessesLeft === 0) {
-      console.log("Sorry, you lose");
+      console.log("Sorry, you lose.");
       losses++;
       document.getElementById("loss-counter").innerHTML = losses;
       document.getElementById("message").innerHTML = "Sorry, you lose.";

73571ba5f1bf3d524c6c23261006ed7c1892b711
ANS <36783010+answebdev@users.noreply.github.com>
2018-08-24 18:34:21 -0700

    added win and lose messages

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,9 @@ var randomWord;
 
 // Variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
+
+var message = document.getElementById("message");
+
 var wordBlanks = document.getElementById("word-blanks");
 var wrongGuesses = document.getElementById("wrong-guesses");
 var guessesLeft = document.getElementById("guesses-left");
@@ -69,64 +72,78 @@ document.onkeyup = function (event) {
   // Hide the directions
   directionsText.textContent = "";
 
-    // CHECK THE GUESS
-    if (randomWord.indexOf(userGuess) > -1) {
-      for (var i = 0; i < randomWord.length; i++) {
-        if (randomWord[i] === userGuess) {
-          spaces[i] = userGuess;
-          console.log(spaces);
-          // rightLetter.push(userGuess);
-          console.log(rightLetter);
-          // Populate the game (spaces) with the correctly guessed letter
-          document.getElementById("word-blanks").innerHTML = spaces.join("");
-        }
-      }
-      console.log(randomWord.split(""));
-      console.log(spaces);
-
-      // CODE THAT DETERMINES WIN OR LOSS
-      //correct user guess (spaces array) === random word (randomWord is a string, so in order for it to be an array so that it's equal to correct user guess, also an array, we use split - join turns an array into a string)
-      var matched = true;
-      for (i = 0; i < spaces.length; i++) {
-
-        if (spaces[i] !== (randomWord.split(""))[i]) {
-          matched = false;
-        }
-      }
-      if (matched === true) {
-        console.log("You win!");
-        wins++;
-        document.getElementById("win-counter").innerHTML = wins;
-        console.log(wins);
-        reset();
-      }
-    } else {
-      // Push wrong guess into wrongLetter array
-      wrongLetter.push(userGuess);
-      // Show which letters were guessed incorrectly on the screen
-      document.getElementById("wrong-guesses").innerHTML = wrongLetter.join(", ");
-      // Decrease the number of guesses left
-      guessesLeft--;
-      // Update number of guesses left on the screen
-      document.getElementById("guesses-left").innerHTML = guessesLeft;
-      console.log(guessesLeft);
-      console.log(wrongLetter);
-
-      if (guessesLeft === 0) {
-        console.log("You lose");
-        losses++;
-        document.getElementById("loss-counter").innerHTML = losses;
-        console.log(losses);
-        reset();
+  // CHECK THE GUESS
+  if (randomWord.indexOf(userGuess) > -1) {
+    for (var i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] === userGuess) {
+        spaces[i] = userGuess;
+        console.log(spaces);
+        // rightLetter.push(userGuess);
+        console.log(rightLetter);
+        // Populate the game (spaces) with the correctly guessed letter
+        document.getElementById("word-blanks").innerHTML = spaces.join("");
       }
     }
+    console.log(randomWord.split(""));
+    console.log(spaces);
+
+    // CODE THAT DETERMINES WIN OR LOSS
+    //correct user guess (spaces array) === random word (randomWord is a string, so in order for it to be an array so that it's equal to correct user guess, also an array, we use split - join turns an array into a string)
+    var matched = true;
+    for (i = 0; i < spaces.length; i++) {
+
+      if (spaces[i] !== (randomWord.split(""))[i]) {
+        matched = false;
+      }
+    }
+    if (matched === true) {
+      console.log("You win!");
+      wins++;
+      document.getElementById("win-counter").innerHTML = wins;
+      // alert("You win!");
+      document.getElementById("message").innerHTML = "You win!";
+      // // Add win image
+      // var img = document.createElement("img");
+      // img.src = "assets/images/you-win.png";
+      // var src = document.getElementById("win-message");
+      // src.appendChild(img);
+      console.log(wins);
+      reset();
+    }
+  } else {
+    // Push wrong guess into wrongLetter array
+    wrongLetter.push(userGuess);
+    // Show which letters were guessed incorrectly on the screen
+    document.getElementById("wrong-guesses").innerHTML = wrongLetter.join(", ");
+    // Decrease the number of guesses left
+    guessesLeft--;
+    // Update number of guesses left on the screen
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    console.log(guessesLeft);
+    console.log(wrongLetter);
+
+    if (guessesLeft === 0) {
+      console.log("You lose");
+      losses++;
+      document.getElementById("loss-counter").innerHTML = losses;
+      // alert("Sorry, you lose.");
+      document.getElementById("message").innerHTML = "Sorry, you lose.";
+      // Add lose image
+      // var img = document.createElement("img");
+      // img.src = "assets/images/you-lose.png";
+      // var src = document.getElementById("lose-message");
+      // src.appendChild(img);
+      console.log(losses);
+      reset();
+    }
   }
+}
 
 // RESET THE GAME
 function reset() {
+  // message.textContent == "";
   guessesLeft = 9;
   document.getElementById("guesses-left").innerHTML = guessesLeft;
-  console.log(guessesLeft);
   rightLetter = [];
   wrongLetter = [];
   document.getElementById("wrong-guesses").innerHTML = wrongLetter.join(", ");
